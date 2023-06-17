a4d11db6a440323ff8b2ebeb058381290a64afe6
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-17 11:06:53 +0300

    Created char counter and added logic

--- StateAnaDataFlow/LoginView.swift
+++ StateAnaDataFlow/LoginView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct LoginView: View {
     @State private var name = ""
+    @State private var charCount = 0
     @EnvironmentObject private var user: UserSettings
     
     var body: some View {
         VStack {
             TextField("Enter your name...", text: $name)
                 .multilineTextAlignment(.center)
+                .onChange(of: name) { newValue in
+                    charCount = newValue.count
+                }
             Button(action: login) {
                 HStack {
                     Image(systemName: "checkmark.circle")
                     Text("OK")
+                    Text("\(charCount)")
+                        .foregroundColor(textColor)
                 }
             }
         }
@@ -30,6 +36,11 @@ struct LoginView: View {
             user.isLoggedIn.toggle()
         }
     }
+    
+    private var textColor: Color {
+            return charCount >= 3 ? .green : .red
+        }
+    
 }
 
 struct LoginView_Previews: PreviewProvider {
